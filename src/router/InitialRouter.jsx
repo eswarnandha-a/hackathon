8a8251841d5d251a2cce91c9b03dc49cd56ae670
eswar-anand <eswaranand1999@gmail.com>
2025-03-16 23:03:09 +0530

    worked on router1

--- src/router/InitialRouter.jsx
+++ src/router/InitialRouter.jsx
@@ -14,6 +14,7 @@ import Layout from '../components/Layout';
 const InitialRouter = ({ token, user, handleLogin, handleLogout, handleQuestionnaireComplete }) => {
   const PrivateRoute = ({ children }) => {
     if (!token) {
+      console.log("")
       return <Navigate to="/" />;
     }
     if (!user.isQuestionnaireDone && window.location.pathname !== '/questionnaire') {
